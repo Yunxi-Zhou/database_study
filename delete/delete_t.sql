3880df903b30680810f14efb07d4f52735df384f
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-11-19 22:50:40 -0500

    delete test

---
+++ delete/delete_t.sql
@@ -0,0 +1,24 @@
+-- SELECT * FROM reviews
+-- DELETE FROM reviews WHERE rid = 3;
+-- SELECT * FROM reviews;
+
+-- INSERT INTO reviews
+-- VALUES(2,3,10,10,'Superb!');
+-- SELECT * FROM reviews;
+
+-- DELETE FROM reviews WHERE pid = rid;
+-- SELECT * FROM reviews;
+
+-- INSERT INTO reviews
+-- VALUES(1,1,6,7,'Great!');
+-- SELECT * FROM reviews;
+
+-- DELETE FROM reviews;
+-- SELECT * FROM reviews;
+
+-- INSERT INTO reviews
+-- VALUES (1,1,6,7,'Great!'),
+--         (1,2,9,3,'Amazing!'),
+--         (2,1,4,9,'Perfect!'),
+--         (2,3,10,10,'Superb!');
+-- SELECT * FROM reviews;
