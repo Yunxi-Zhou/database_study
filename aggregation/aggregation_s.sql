40b1f1cb87d57d8ec3143d7ecce6657033e8b1be
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-11-26 16:41:55 -0500

    structure of aggregated queries

--- aggregation/aggregation_s.sql
+++ aggregation/aggregation_s.sql
@@ -3,4 +3,4 @@ SELECT aggregated output columns
 FROM sources
 WHERE row conditions  -- Filter rows before grouping
 GROUP BY columns to group rows on
-HAVING conditions on aggregated groups;
+HAVING conditions on aggregated groups;  -- Filters aggregated rows after grouping
