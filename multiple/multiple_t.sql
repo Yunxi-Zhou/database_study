0c49547cb238de46e60d3ef4133765730a2e95aa
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-11-26 14:23:17 -0500

    multiple table test

---
+++ multiple/multiple_t.sql
@@ -0,0 +1,58 @@
+-- SELECT * FROM courses2;
+-- SELECT * FROM instructors;
+
+-- SELECT *
+-- FROM courses2 C, instructors I
+-- WHERE C.cid = I.cid;
+
+-- SELECT *
+-- FROM courses2 C CROSS JOIN instructors I
+-- WHERE C.cid = I.cid;
+
+-- SELECT * 
+-- FROM courses2 C JOIN 
+-- instructors I ON C.cid = I.cid;
+
+-- SELECT * 
+-- FROM courses2 C INNER JOIN
+--     instructors I ON C.cid = I.cid;
+
+-- SELECT *
+-- FROM courses2 C LEFT JOIN
+--     instructors I ON C.cid = I.cid;
+
+-- SELECT * FROM courses2
+
+-- SELECT * 
+-- FROM courses2 C LEFT OUTER JOIN 
+--     instructors I ON C.cid = I.cid
+
+-- SELECT *
+-- FROM courses2 C RIGHT JOIN
+--     instructors I ON C.cid = I.cid;
+
+-- SELECT *
+-- FROM courses2 C RIGHT OUTER JOIN
+--     instructors I ON C.cid = I.cid;
+
+-- SELECT *
+-- FROM courses2 C FULL JOIN
+--     instructors I ON C.cid = I.cid;
+
+-- SELECT *
+-- FROM courses2 C 
+-- LEFT JOIN instructors I ON C.cid = I.cid
+-- UNION
+-- SELECT *
+-- FROM courses2 C 
+-- RIGHT JOIN instructors I ON C.cid = I.cid;
+
+SELECT *
+FROM courses2 C RIGHT JOIN 
+    instructors I ON C.cid > I.cid
+
+UNION
+
+SELECT *
+FROM courses2 C LEFT JOIN
+    instructors I ON C.cid > I.cid;
