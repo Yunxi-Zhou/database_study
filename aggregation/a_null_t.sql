9b232fd4b9a4d0c1ed004716e38f4db7cb398c9d
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-11-26 18:31:34 -0500

    aggregation null test

---
+++ aggregation/a_null_t.sql
@@ -0,0 +1,50 @@
+-- SELECT * FROM productreview
+
+-- SELECT MIN(rating)
+-- FROM productreview_n;
+
+-- SELECT COUNT(rating)
+-- FROM productreview_n;
+
+-- SELECT COUNT(*)
+-- FROM productreview_n;
+
+-- SELECT COUNT(DISTINCT rating)
+-- FROM productreview_n;
+
+-- SELECT SUM(rating)
+-- FROM productreview_n;
+
+-- SELECT AVG(rating)
+-- FROM productreview_n;
+
+-- SELECT AVG(DISTINCT rating)
+-- FROM productreview_n;
+
+-- SELECT MIN(rating)
+-- FROM productreview_n
+-- WHERE rating IS NULL;
+
+-- SELECT MAX(rating)
+-- FROM productreview_n
+-- WHERE rating IS NULL;
+
+-- SELECT COUNT(rating)
+-- FROM productreview_n
+-- WHERE rating IS NULL;
+
+-- SELECT COUNT(*) 
+-- FROM productreview_n
+-- WHERE rating IS NULL;
+
+-- SELECT SUM(rating)
+-- FROM productreview_n
+-- WHERE rating IS NULL;
+
+-- SELECT AVG(rating)
+-- FROM productreview_n
+-- WHERE rating IS NULL;
+
+-- SELECT AVG(DISTINCT rating)
+-- FROM productreview_n
+-- WHERE rating IS NULL;
