2ed1dc041e8c8748380f38888e9080ecd0b8b399
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-11-26 19:35:50 -0500

    from test

---
+++ from/from_t.sql
@@ -0,0 +1,9 @@
+-- SELECT * FROM courses2;
+-- SELECT * FROM instructors;
+
+-- SELECT * 
+-- FROM (SELECT * FROM courses2 
+--         WHERE cid IN (SELECT cid FROM instructors)) AS C
+-- WHERE title LIKE '%Databases%';
+
+-- SELECT * FROM productreview;
