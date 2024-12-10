a2d64fc4f2d77cca301ffc0a7df0a8ad9f9fa387
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-12-10 11:31:11 -0500

    select the column for quick view

---
+++ feature/check.sql
@@ -0,0 +1,8 @@
+-- Views: Shorthands for SELECT queries
+-- Views can be used to define shorthands:
+-- Views act like tables, but are defined by a SELECT statement:
+
+CREATE VIEW viewname
+AS SELECT query;
+
+-- Simple views are typically updatable
