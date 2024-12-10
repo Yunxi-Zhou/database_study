ac92fb24ab80edf002b3f45ea12df8c0bf46b88b
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-12-10 11:25:57 -0500

    altering and droping table

---
+++ feature/alter.sql
@@ -0,0 +1,9 @@
+-- ALTERing and DROPing tables
+
+-- Tables can be modified via ALTER TABLE 
+-- column can be added, changed or removed
+-- table constraints can be added, changed or removed
+-- other changes can be made
+
+ALTER TABLE cars
+ALTER COLUMN year TYPE VARCHAR(4);
