b3f255c70c075b2ae5027a852c6324d58b472df1
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2024-12-17 16:54:18 -0500

    bag semantics

---
+++ relational_algebra/multiset.sql
@@ -0,0 +1,9 @@
+-- Extension: Bag(multiset) semantics
+
+-- Impact on operators
+-- Unaffected 𝜎c, 𝜌R, ×, ZC, Z.
+-- Affected ∪, ∩, \, 𝛾 (⋉, ⋊).
+-- Strongly affected 𝜋D.
+
+-- Syntax of the deduplication operator
+-- 𝛿 (expression e)
